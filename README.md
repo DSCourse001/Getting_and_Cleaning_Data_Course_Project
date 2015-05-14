65f6d50a212d892d5a3642a041e2f77cc8f591a7
DSCourse001 <DSCourse001@github.com>
2015-05-14 18:33:32 +0300

    Added Task Files for Getting and Cleaning Data Course Project

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Getting_and_Cleaning_Data_Course_Project
-Repository for Getting and Cleaning Data Course Project (getdata-014)
+## Getting and Cleaning Data Course Project
+
+### Description
+This a root document for "Getting and Cleaning Data" Course Project set of documents.
+The goal of "Getting and Cleaning Data" Course Project is to write a program on R language that reads Source Data from text files and produces one text file with tidy data. 
+
+General principles of tidy data is explained in [this paper](http://vita.had.co.nz/papers/tidy-data.pdf) by Hadley Wickham.
+
+This documents also contain information about other documents related to "Getting and Cleaning Data" Course Project and steps to execute R program.
+
+### Related Documents
+
+1. [README.md](README.md) - This file. The root document.
+1. [CodeBook.md](CodeBook.md) - Code Book which describes Source Data and steps to clear up data.
+1. [run_analysis.R](run_analysis.R) - R Program which do all work.
+
+### How to Execute R Program
+The best way to execute R Programs is to use [RStudio IDE](http://www.rstudio.com). [This is direct link to download page] (http://www.rstudio.com/products/rstudio/download/).
+
+* After you have installed R environment on your box you need to set up Working Directory.
+Navigation in RStudio:
+      Menu -> Session -> Set Working Directory -> Choose Directory
+  Or you can execute setwd() function in the R Console.
+```{r}
+setwd("<Path_To_Your_Working_Directory>")
+```
+
+* Now this is a time to prepare Source Data manually by downloading from [this link] (https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip) and extracting in the Working Directory. Actually, R Program will try to download and extract archive with Source Data to your working directory (if it is not exists), but to be sure you can do this manually.
+
+* Just source the R Program file.
+
+```{r}
+source("run_analysis.R")
+```
+
+* Script should produce "tidy_data.txt" text file in your Working Directory.
---
+++ run_analysis.R
@@ -0,0 +1,104 @@
+# Script will produce "tidy_data.txt" file with tidy data from source.
+# Source Data neet to be downloaded from the URL:
+#   https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+# 
+# After that you should extract archive to R Working Directory.
+# Folder with name "UCI HAR Dataset" should be created.
+# 
+# You can do this task manually or script will download and extract archive for you.
+#
+# After script execution R memory will contain "tidy_data" data.table
+# You can use it for further analysis or remove.
+#
+# If you have Source Data in directory then this script should run 
+# neally 3-5 seconds on modern desctop PC (2015 year). This is mainly beacause of 
+# use "data.table" and "LaF" packages. 
+# 
+
+# Install Required Packages if we cannot load it
+if (!require("data.table")) install.packages("data.table")
+if (!require("LaF")) install.packages("LaF")
+if (!require("dplyr")) install.packages("dplyr")
+
+# Load packages
+require("data.table")
+require("LaF")
+require("dplyr")
+
+# If "UCI HAR Dataset" directory is not exists
+#   then download ZIP archive and extract it
+if (!file.exists("UCI HAR Dataset")) {
+  url="https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+  tmpZIPFile <- tempfile()
+  download.file(url=url,destfile=tmpZIPFile,method="wget",quiet=T,mode="wb",extra=c("--no-check-certificate"))
+  unzip(tmpZIPFile,overwrite = TRUE)
+  unlink(tmpZIPFile)
+}
+
+# Getting Labels for Features and Activities
+# File is really small, no metter use "fread" or "read.table"
+actLab<-read.table("./UCI HAR Dataset/activity_labels.txt",header=F,colClasses="character")
+setnames(actLab,1:2,c("Id","ActName"))
+
+ftrLab<-read.table("./UCI HAR Dataset/features.txt",header=F,colClasses="character")
+setnames(ftrLab,1:2,c("Id","ColName"))
+colNums<-nrow(ftrLab)
+
+# Step 1. Merges the training and the test sets to create one data set
+
+# Fast Load Fixed Length Data (runs 3 times faster than read.table)
+# You cannot load this file using "fread", it just will crush.
+testlaf=laf_open_fwf(
+  filename="./UCI HAR Dataset/test/X_test.txt"
+  ,column_types=rep("numeric",colNums)
+  ,column_widths=rep(16,colNums))
+test<-testlaf[,]
+close(testlaf)
+setnames(test,1:ncol(test),ftrLab$ColName)
+
+# Loading smal files
+test$Activity<-read.table("./UCI HAR Dataset/test/Y_test.txt",header=F,colClasses="numeric")[,1]
+test$Subject<-read.table("./UCI HAR Dataset/test/subject_test.txt",header=F,colClasses="numeric")[,1]
+
+# Using LaF package again
+trainlaf=laf_open_fwf(
+  filename="./UCI HAR Dataset/train/X_train.txt"
+  ,column_types=rep("numeric",colNums)
+  ,column_widths=rep(16,colNums))
+train<-trainlaf[,]
+close(trainlaf)
+setnames(train,1:ncol(train),ftrLab$ColName)
+
+train$Activity<-read.table("./UCI HAR Dataset/train/Y_train.txt",header=F,colClasses="numeric")[,1]
+train$Subject<-read.table("./UCI HAR Dataset/train/subject_train.txt",header=F,colClasses="numeric")[,1]
+
+data<-data.table(rbindlist(list(test,train),use.names=TRUE))
+setcolorder(data,c(colNums+1,colNums+2,1:colNums))
+rm(test,train,ftrLab,colNums)
+
+# Step 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+data<-data[,grep("(^(Activity|Subject)$|(mean|std)\\(\\))",names(data)),with=F]
+
+# Step 3. Uses descriptive activity names to name the activities in the data set
+data$Activity<-factor(data$Activity,labels=actLab$ActName)
+rm(actLab)
+
+# Step 4. Appropriately labels the data set with descriptive variable names.
+data$Subject<-factor(data$Subject)
+
+# Step 5. From the data set in step 4, creates a second, independent tidy data 
+#     set with the average of each variable for each activity and each subject.
+
+# Set Grouping Element and Order by Grouping Element
+data <- data %>% 
+  group_by(Activity,Subject) %>%
+    arrange(Activity,Subject)  
+
+# Getting Average for each column by group
+tidy_data<-summarise_each(data,funs(mean))
+rm(data)
+
+# Write Data to File "tidy_data.txt"
+write.table(tidy_data,"tidy_data.txt",row.name=FALSE)
+
